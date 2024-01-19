7a432f4c9b6cf2efbd42f71a3c7927b3d72a3740
Marcel Boersma <m.boersma@uva.nl>
2024-01-19 07:56:02 +0100

    Create FLUE.py

---
+++ fintorch/datasets/FLUE.py
@@ -0,0 +1,4 @@
+# FLUE Benchmark: 
+#
+# A heterogeneous benchmark built from 5 diverse financial domain-specific datasets, 
+# encompassing areas like sentiment classification, question answering, and named entity recognition.
