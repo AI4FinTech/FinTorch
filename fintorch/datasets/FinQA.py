0b4ab9425a64e2397ef63a63c3352471fcae698e
Marcel Boersma <m.boersma@uva.nl>
2024-01-19 07:53:26 +0100

    Create FinQA.py

---
+++ fintorch/datasets/FinQA.py
@@ -0,0 +1,4 @@
+# Financial Reports Dataset: 
+# 
+# Featuring 2.8k financial reports and 8k Q&A pairs, this dataset is tailored for studies in numerical reasoning, 
+# incorporating both structured and unstructured data.
