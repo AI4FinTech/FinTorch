caa4fe80e6a4d4b820e98777f12e78a06011865c
Marcel Boersma <m.boersma@uva.nl>
2024-01-19 18:35:57 +0100

    Create OrderBook.py

---
+++ fintorch/datasets/OrderBook.py
@@ -0,0 +1,3 @@
+# Limit Order Book Data for Mid-Price Forecasting: 
+# A high-frequency dataset from NASDAQ Nordic stock market, 
+# ideal for mid-price prediction and financial time series analysis.
