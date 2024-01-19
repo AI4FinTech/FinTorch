fdc86062f8075ca069e57d6f717e3150fde24b7c
Marcel Boersma <m.boersma@uva.nl>
2024-01-19 18:50:01 +0100

    Create AmazonFraudDataset.py

---
+++ fintorch/datasets/AmazonFraudDataset.py
@@ -0,0 +1,3 @@
+# Amazon-Fraud is a multi-relational graph dataset built upon the Amazon review dataset, 
+# which can be used in evaluating graph-based node classification, fraud detection, 
+# and anomaly detection models.
