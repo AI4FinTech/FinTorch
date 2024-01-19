a04fe91c66d9e6c3020d764fdaa947e877096777
Marcel Boersma <m.boersma@uva.nl>
2024-01-19 18:46:40 +0100

    Create ECTSum.py

---
+++ fintorch/datasets/ECTSum.py
@@ -0,0 +1,4 @@
+# ECTSum is a dataset with transcripts of earnings calls (ECTs), 
+# hosted by public companies, as documents, 
+# and short experts-written telegram-style bullet point summaries derived from corresponding Reuters articles. 
+# ECTs are long unstructured documents without any prescribed length limit or format.
