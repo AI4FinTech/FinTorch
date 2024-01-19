547bf9591f569e566b7723af95903cb7ed9dd20a
Marcel Boersma <m.boersma@uva.nl>
2024-01-19 07:54:27 +0100

    Create FINDSum.py

---
+++ fintorch/datasets/FINDSum.py
@@ -0,0 +1,5 @@
+# FINDSum Dataset: 
+#
+# A comprehensive dataset for long text and multi-table summarization, 
+# built on 21,125 annual reports from 3,794 companies. 
+# It focuses on summarizing companies’ operational results and liquidity.
