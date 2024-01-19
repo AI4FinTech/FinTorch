ac5a38655553bd0ebf474d3f7aea4aa1678cff59
Marcel Boersma <m.boersma@uva.nl>
2024-01-19 07:51:09 +0100

    Create CDIP.py

---
+++ fintorch/datasets/CDIP.py
@@ -0,0 +1,4 @@
+# Large-Scale Scanned Document Image Dataset: 
+# 
+# This expansive dataset contains approximately 11 million document images, split into 42 million pages.
+# It's an invaluable resource for projects involving document analysis and processing.
