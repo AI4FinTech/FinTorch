16e1850341df2eafa04f6d567c3c1b388b2996d3
Marcel Boersma <boersma.marcel@gmail.com>
2024-02-13 20:15:57 +0100

    small fixes

---
+++ docs/fintorch.rst
@@ -0,0 +1,37 @@
+fintorch package
+================
+
+Subpackages
+-----------
+
+.. toctree::
+   :maxdepth: 4
+
+   fintorch.dl
+
+Submodules
+----------
+
+fintorch.cli module
+-------------------
+
+.. automodule:: fintorch.cli
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+fintorch.fintorch module
+------------------------
+
+.. automodule:: fintorch.fintorch
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: fintorch
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- setup.py
+++ setup.py
@@ -1,49 +1,52 @@
 #!/usr/bin/env python
-
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = [
+    "Click>=7.0",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Marcel Boersma",
-    author_email='boersma.marcel@gmail.com',
-    python_requires='>=3.6',
+    author_email="boersma.marcel@gmail.com",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="AI4FinTech project repository",
     entry_points={
-        'console_scripts': [
-            'fintorch=fintorch.cli:main',
+        "console_scripts": [
+            "fintorch=fintorch.cli:main",
         ],
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='fintorch',
-    name='fintorch',
-    packages=find_packages(include=['fintorch', 'fintorch.*']),
-    test_suite='tests',
+    keywords="fintorch",
+    name="fintorch",
+    packages=find_packages(include=["fintorch", "fintorch.*"]),
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/boersmamarcel/fintorch',
-    version='0.1.0',
+    url="https://github.com/boersmamarcel/fintorch",
+    version="0.1.0",
     zip_safe=False,
 )
