89311ea144b36cb60337b350efb0accd0574db11
boersmamarcel <boersma.marcel@gmail.com>
2024-05-15 15:43:27 +0200

    Update model

--- examples/models/graphbean.py
+++ examples/models/graphbean.py
@@ -1,42 +1,27 @@
 import lightning as L
-import torch
-from torch_geometric.data import HeteroData
+from torch_geometric.nn.conv import TransformerConv
 
-from fintorch.models.graph.graphBEAN import GraphBEANLoss, GraphBEANModule
+from fintorch.datasets.ellipticpp import EllipticDataModule
+from fintorch.models.graph.graphBEAN import GraphBEANModule
 
-# Create feature matrices for node types A and B
-x = HeteroData()
-x["A"].x = torch.randn(100, 10)
-x["B"].x = torch.randn(200, 20)
-
-# Add feature dimension 'y' for node type A and B with random assignment of class A, B, and C
-x["A"].y = torch.randint(0, 3, (100, ))
-x["B"].y = torch.randint(0, 3, (100, ))
-
-# Add edge index for connections between nodes of A and B
-# TODO: fix edge construction
-edge_index = torch.randint(0, 100, (2, 200))
-x["A", "to", "B"].edge_index = edge_index
-x["B", "to", "A"].edge_index = edge_index
-
-print(f"Heterodata: {x}")
+# We use an example data module from the elliptic dataset which is bipartite
+data_module = EllipticDataModule(("wallets", "to", "transactions"))
 
 # Create an instance of the GraphBEANModule
 module = GraphBEANModule(
-    ("A", "to", "B"),
-    loss_fn=GraphBEANLoss,
+    ("wallets", "to", "transactions"),
     learning_rate=0.001,
-    encoder_layers=2,
-    data=x,
-    classifier=False,
+    classifier=True,
+    conv_type=TransformerConv,
+    encoder_layers=10,
+    decoder_layers=10,
+    hidden_layers=100,
+    class_head_layers=4,
+    predict="transactions",
 )
 
 # Create a PyTorch Lightning Trainer and train the module
-trainer = L.Trainer(max_epochs=10)
-
-# Use the dataset to create train and validation dataloaders
-train_dataloader = x
-val_dataloader = x
+trainer = L.Trainer(max_epochs=10, accelerator="gpu")
 
 # Train the module using the dataloaders
-trainer.fit(module, x)
+trainer.fit(module, datamodule=data_module)
