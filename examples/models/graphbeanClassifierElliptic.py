25a111c5d4a71600b12c65166cefa7c3416e1554
boersmamarcel <boersma.marcel@gmail.com>
2024-06-15 10:38:26 +0200

    Fixes windows bug for multiprocessor use

--- examples/models/graphbeanClassifierElliptic.py
+++ examples/models/graphbeanClassifierElliptic.py
@@ -7,28 +7,36 @@ from fintorch.models.graph.graphbean.graphBEAN import GraphBEANModule
 
 torch.set_float32_matmul_precision("medium")
 
-# We use an example data module from the elliptic dataset which is bipartite
-data_module = EllipticppDataModule(("transactions", "to", "wallets"),
-                                   force_reload=False)
 
-# Create an instance of the GraphBEANModule
-module = GraphBEANModule(
-    ("transactions", "to", "wallets"),
-    edge_types=[("wallets", "to", "transactions"),
-                ("transactions", "to", "wallets")],
-    learning_rate=0.0001,
-    conv_type=BEANConvSimple,
-    encoder_layers=5,
-    decoder_layers=5,
-    hidden_layers=512,
-    classes=2,
-    class_head_layers=100,
-    classifier=True,
-    predict="wallets",
-)
+def main():
+    # We use an example data module from the elliptic dataset which is bipartite
+    data_module = EllipticppDataModule(("transactions", "to", "wallets"),
+                                       force_reload=False)
 
-# Create a PyTorch Lightning Trainer and train the module
-trainer = L.Trainer(max_epochs=100, accelerator="gpu")
+    # Create an instance of the GraphBEANModule
+    module = GraphBEANModule(
+        ("transactions", "to", "wallets"),
+        edge_types=[
+            ("wallets", "to", "transactions"),
+            ("transactions", "to", "wallets"),
+        ],
+        learning_rate=0.0001,
+        conv_type=BEANConvSimple,
+        encoder_layers=5,
+        decoder_layers=5,
+        hidden_layers=512,
+        classes=2,
+        class_head_layers=100,
+        classifier=True,
+        predict="wallets",
+    )
 
-# Train the module using the dataloaders
-trainer.fit(module, datamodule=data_module)
+    # Create a PyTorch Lightning Trainer and train the module
+    trainer = L.Trainer(max_epochs=100, accelerator="gpu")
+
+    # Train the module using the dataloaders
+    trainer.fit(module, datamodule=data_module)
+
+
+if __name__ == "__main__":
+    main()
