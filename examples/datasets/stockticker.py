8eea1b420c8015d00c499b528c4a7eae9bd045f6
boersmamarcel <boersma.marcel@gmail.com>
2024-06-22 08:36:22 +0200

    Down stock ticks dataset

---
+++ examples/datasets/stockticker.py
@@ -0,0 +1,23 @@
+from fintorch.datasets import stockticker
+
+# Load the stock dataset
+stockdata = stockticker.StockTicker('~/.fintorch_data')
+
+# class GNN(torch.nn.Module):
+
+#     def __init__(self, hidden_channels, out_channels):
+#         super().__init__()
+#         self.conv1 = SAGEConv((-1, -1), hidden_channels)
+#         self.conv2 = SAGEConv((-1, -1), out_channels)
+
+#     def forward(self, x, edge_index):
+#         x = self.conv1(x, edge_index)
+#         x = self.conv2(x, edge_index)
+#         return x
+
+# model = GNN(hidden_channels=64, out_channels=3)
+# model = to_hetero(model, data.metadata(), aggr='sum')
+
+# with torch.no_grad():
+#     out = model(data.x_dict, data.edge_index_dict)
+#     print(out)
