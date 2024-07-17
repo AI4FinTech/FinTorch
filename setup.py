c930836da28fa734ad5d90f7cbadeb545e737e7f
boersmamarcel <boersma.marcel@gmail.com>
2024-07-17 09:13:43 +0200

    Bump version: 0.1.13 → 0.1.14

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/AI4FinTech/fintorch",
-    version="0.1.13",
+    version="0.1.14",
     zip_safe=False,
 )
