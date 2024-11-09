e9c756057492bfd6cc89957bd620d5d1aa59abb0
boersmamarcel <boersma.marcel@gmail.com>
2024-11-09 14:09:19 +0100

    Code optimization

--- fintorch/datasets/stockticker.py
+++ fintorch/datasets/stockticker.py
@@ -138,7 +138,7 @@ class StockTicker(Dataset):  # type: ignore
             "spatial_graph_v1.pt",
         ]
 
-    @retry(
+    @retry(  # type: ignore[misc]
         stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10)
     )
     def download_with_retry(
