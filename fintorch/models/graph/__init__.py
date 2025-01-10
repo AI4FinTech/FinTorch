ed0146aba5087b2dbe3c0b628db3d7b352c29557
Marcel Boersma <Boersma.marcel@kpmg.nl>
2025-01-10 18:22:50 +0100

    Fixes init

--- fintorch/models/graph/__init__.py
+++ fintorch/models/graph/__init__.py
@@ -1,15 +1,4 @@
-from .graphbean import (
-    GraphBEANLoss,
-    GraphBEANLossClassifier,
-    StructureDecoder,
-    GraphBean,
-    GraphBeanClassifier,
-    GraphBEANModule,
-)
-
 __all__ = [
-    "GraphBEANLoss",
-    "GraphBEANLossClassifier",
     "StructureDecoder",
     "GraphBean",
     "GraphBeanClassifier",
