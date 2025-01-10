4a5685d0915a23ba6f527c175764ec84e2300df5
Marcel Boersma <Boersma.marcel@kpmg.nl>
2025-01-10 22:43:30 +0100

    Small modifications for split

--- examples/datasets/example_invoicedata.py
+++ examples/datasets/example_invoicedata.py
@@ -6,7 +6,7 @@ from fintorch.datasets.invoice import InvoiceDataset
 logging.basicConfig(level=logging.INFO)
 
 data_path = Path("~/.fintorch_data/invoice-data/").expanduser()
-auction_data = InvoiceDataset(data_path, False)
+auction_data = InvoiceDataset(data_path, force_reload=False)
 
 print(f"Length of the dataset:{len(auction_data)} \n Print first 10 records:")
 
